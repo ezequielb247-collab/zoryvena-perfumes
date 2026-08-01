91551019e1c01ba7f40b34087e29d76ba4e19f0f
ezequielb247-collab <ezequielb247@gmail.com>
2026-08-01 14:46:17 -0300

    Publica página de retorno do pagamento

--- render-build.sh
+++ render-build.sh
@@ -6,7 +6,7 @@ mkdir -p dist
 
 # Publica os arquivos necessários para a loja e para o painel administrativo demonstrativo.
 cp -R assets politicas produto admin dist/
-cp index.html catalogo.html carrinho.html checkout.html comparar.html contato.html quiz.html sobre.html 404.html dist/
+cp index.html catalogo.html carrinho.html checkout.html pagamento.html comparar.html contato.html quiz.html sobre.html 404.html dist/
 cp manifest.webmanifest robots.txt sitemap.xml dist/
 
 printf 'Build da Zoryvena criado em dist/ com loja e painel administrativo.\n'
